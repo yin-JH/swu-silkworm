6a4ad9d4d3a1f96900bfc845b7184f5c120d9d8e
kit <kakit0713@163.com>
2021-03-08 21:10:37 +0800

    web修改1.2

--- src/main/resources/static/js/webPages.js
+++ src/main/resources/static/js/webPages.js
@@ -1,4 +1,4 @@
-function getNavigation() {
+function getNavigation() {/* js 生成导航栏 */
     var a = "";
     a = "<nav class=\"navbar navbar-default\">\n" +
         "    <div class=\"container-fluid\">\n" +
@@ -38,7 +38,7 @@ function getNavigation() {
     /*window.alert(window.location.pathname);*/
     if (window.location.pathname != "/login"){
         if(window.location.pathname == "/index"){
-            document.getElementById("navi_left").innerHTML="<li class=\"active\"><a href=\"/index\">问答界面</a></li>\n<li><a href=\"/damin\">数据管理</a></li>"
+            document.getElementById("navi_left").innerHTML="<li class=\"active\"><a href=\"/index\">问答界面</a></li>\n<li><a href=\"/admin\">数据管理</a></li>"
         }
         else if (window.location.pathname == "/admin"){
             document.getElementById("navi_left").innerHTML="<li><a href=\"/index\">问答界面</a></li>\n<li class=\"active\"><a href=\"/admin\">数据管理</a></li>"
@@ -46,19 +46,18 @@ function getNavigation() {
     }
 }
 
-function get_data_table() {
+function get_data_table() {/* js 表格数据 */
     /*  动态生成表格 ↓  */
     var tbody = document.querySelector("tbody");
     var i = 1;
     var str;
-    var url = "...";/*接口地址*/
+    var url = "/admin/retrieve";/*接口地址*/
     var args = {};
 
     $.post(url,args,function (res) {
         /*res='{"id"=1,"problem"="1111","media_type"="123","answer"="123456"}';*/
         str = '['+res+']';
-
-        console.log(str);
+        /*console.log(str);*/
 
         var obj = JSON.parse(str);
 
@@ -86,10 +85,71 @@ function get_data_table() {
             var td = document.createElement("td");
             tr.appendChild(td);
             var id = obj[i].id;
-            td.innerHTML = "<button id=\"btn_edit_" + id + "\" name=\"编辑\">";
+            td.innerHTML = "<button id=" + id + " type=\"button\" onclick=\"edit_one(id)\">编辑</button>";
         }
     });
 
     /*  动态生成表格 ↑  */
+}
 
+function edit_one(n) {
+    /*  生成编辑窗口  */
+    var tbody = document.querySelector("tbody");
+    var i = 1;
+    var str;
+    var url = "/admin/retrieve";/*接口地址*/
+    var args = {};
+    var obj;
+
+    if (n !== -1){
+        $.ajaxSettings.async=false;
+        $.post(url,args,function (res) {str=res;/*window.alert(str);*/});
+        $.ajaxSettings.async=true;
+        /*window.alert(str);*/
+        obj = JSON.parse('['+str+']');
+    }
+    else {
+        obj = JSON.parse('[{"id":-1,"problem":"","media_type":"","answer":""}]');
+    }
+     tbody.innerHTML="";
+    for(var i = 0;i < obj.length;i++){
+
+        if (obj[i].id == n){
+            var tr = document.createElement("tr");
+            tbody.appendChild(tr);
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerHTML = 1+i;
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerHTML = "<div class=\"input-group\">\n                   <input id=\"input_problem\" type=\"text\" class=\"form-control\" value=\""+obj[i].problem+"\" aria-describedby=\"basic-addon1\"> </div>";
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerHTML = "<div class=\"input-group\">\n                   <input id=\"input_media_type\" type=\"text\" class=\"form-control\" value= \""+obj[i].media_type+"\" aria-describedby=\"basic-addon1\"> </div>";
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            td.innerHTML = "<div class=\"input-group\">\n                   <input id=\"input_answer\" type=\"text\" class=\"form-control\" value=\""+obj[i].answer+"\" aria-describedby=\"basic-addon1\"> </div>";
+
+            var td = document.createElement("td");
+            tr.appendChild(td);
+            var id = obj[i].id;
+            td.innerHTML = "<button id="+id+" type=\"submit\" onclick=\"submit_data(id,document.getElementById(\"input_problem\").innerText,document.getElementById(\"input_media_type\").innerText,document.getElementById(\"input_answer\").innerText)\">提交</button>";
+
+        }
+    }
+
+}
+
+function submit_data(id,problem,media_type,answer) {
+    var url="";
+    $.ajax(url,{
+        async:false,
+        dataType:"json",
+        data:{"id":id,"problem":problem,"media_type":media_type,"answer":answer},
+        success:window.alert("提交成功！")
+    });
 }
